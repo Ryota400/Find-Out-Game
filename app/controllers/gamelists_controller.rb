dddb2b3e9fc1af39f7bb2aa28164539c3cfdfacb
井上涼太 <inoueryouta@inoueryoutas-MacBook-Air.local>
2022-02-28 22:41:21 +0900

    投稿,編集,削除の追加

--- app/controllers/gamelists_controller.rb
+++ app/controllers/gamelists_controller.rb
@@ -1,18 +1,19 @@
 class GamelistsController < ApplicationController
+  before_action :twitter_client, only: [:create]
 
   def index
     @gamelists = Gamelist.all.includes(:user).order(created_at: :desc)
   end
 
   def new
-    @gamelist = gamelist.new
+    @gamelist = Gamelist.new
   end
 
   def create
     @gamelist = current_user.gamelists.build(gamelist_params)
     if @gamelist.save
-      @client.update("例）投稿しました！（ここにTwitterにツイートされる内容を書き込む）")
-      redirect_to gamelists_path
+      @client.update("#{@gamelist.title} #{gamelists_url(@gamelist.id)}\r")
+      redirect_to gamelists_path, success: t('defaults.message.created', item: Gamelist.model_name.human)
     else
       flash.now['danger'] = t('defaults.message.not_created', item: Gamelist.model_name.human)
       render :new
@@ -23,13 +24,33 @@ class GamelistsController < ApplicationController
     @gamelist = Gamelist.find(params[:id])
   end
 
+  def edit
+    @gamelist = current_user.gamelists.find(params[:id])
+  end
+
+  def update
+    @gamelist = current_user.gamelists.find(params[:id])
+    if @gamelist.update(gamelist_params)
+      redirect_to @gamelist, success: t('defaults.message.updated', item: Gamelist.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Gamelist.model_name.human)
+      render :edit
+    end
+  end
+
+  def destroy
+    @gamelist =current_user.gamelists.find(params[:id])
+    @gamelist.destroy!
+    redirect_to gamelists_path, success: t('defaults.message.deleted', item: Gamelist.model_name.human)
+  end
+
   private
 
   def gamelist_params
     params.require(:gamelist).permit(:title, :body)
   end
 
-  def twitter_cilent
+  def twitter_client
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['TWITTER_API_KEY']
       config.consumer_secret = ENV['TWITTER_API_SECRET']
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   get 'home/show'
-  resources :gamelists, only: %i[index new create]
+  resources :gamelists
 end
